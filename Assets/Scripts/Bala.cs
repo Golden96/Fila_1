b1f824d389a2eba3f8cdb443a323912149d12f69
Sergio Gómez <sergio64gr@gmail.com>
2014-11-17 18:32:25 +0100

    #8 Crear CameraFollow, Disparo para asignar al personaje, excepto el personaje

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bala : MonoBehaviour {
+	
+	public Vector2 velocity = new Vector2(5,0);
+	public GameObject particulas;
+	public GameObject blood;
+	public int damage = 5;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = velocity * transform.localScale.x;
+		//Invoke ("explotaBala", 0.2f);
+	}
+	
+	
+	
+	void explotaBala(Color? color = null ){				
+		var clone = Instantiate(particulas,transform.position,Quaternion.identity) as GameObject;
+		clone.transform.parent = null;
+		clone.particleSystem.startColor = color ?? Color.grey;
+		Destroy(clone,1);
+		Destroy(gameObject);
+	}
+	
+	void OnCollisionEnter2D(Collision2D target){
+		/*
+		if (target.transform.tag == "Zombie") {
+			explotaBala(Color.green);
+			var clone2 =Instantiate(blood,transform.position,Random.rotation) as GameObject;
+		} else {
+		}*/
+		
+		explotaBala();
+	}
+	
+}
